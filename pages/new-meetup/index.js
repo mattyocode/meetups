2c8e0424dee35e942fb8ad2def336619f8edd22e
mattyocode <matthewoliver@live.co.uk>
2021-07-13 16:09:34 +0100

    add post request to new-meetup/index

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -3,8 +3,20 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  async function addMeetupHandler(enteredMeetupData) {
+    // internal API hosted by same server so path to file
+    // second arg to fetch is config obj
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
