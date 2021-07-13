695dd061fd492558f68ad618a372f1ffacc52c78
mattyocode <matthewoliver@live.co.uk>
2021-07-13 15:53:01 +0100

    add new-meetup API route

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+
+const user = process.env.MONGO_USER;
+const pw = process.env.MONGO_PASSWORD;
+const dbName = process.env.MONGO_DB_NAME;
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    // connect returns a promise, so can turn into async function
+    const client = await MongoClient.connect(
+      `mongodb+srv://${user}:${pw}@cluster0.1ykyz.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    // response object
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
